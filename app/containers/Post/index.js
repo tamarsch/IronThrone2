6c785e615af02ff4569f80adea0a53b1b01748f0
Yarden Halperin <yarden.halperin@ironsrc.com>
2017-11-30 13:11:25 +0200

    commit design

--- app/containers/Post/index.js
+++ app/containers/Post/index.js
@@ -5,37 +5,50 @@
  */
 
 import React from 'react';
+import 'style.css';
 import PropTypes from 'prop-types';
 import Comment from '../../components/Comment';
 
 
 export default class Post extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  getTypeOfRequest() {
+    if (this.props.postObj.type === 'help_request') {
+      return 'Number of helpers';
+    }
+    return 'Number people for help';
+  }
   render() {
     return (
-      <div>
-        <h3>{this.props.postObj.title}</h3>
-        <p>{this.props.postObj.user.name}</p>
-        <p>{this.props.postObj.type}</p>
-        <p>{this.props.postObj.category.name}</p>
-        <p>{this.props.postObj.open_date}</p>
-        <p>{this.props.postObj.description}</p>
-        <p>{this.props.postObj.difficulty}</p>
-        <p>{this.props.postObj.duration}</p>
-        <p>{this.props.postObj.help_date}</p>
-        <p>{this.props.postObj.location}</p>
-        <p>{this.props.postObj.number_of_helpers_needed}</p>
-        <p>{this.props.postObj.score}</p>
-        <p>{this.props.postObj.status}</p>
+      <div className={`post ${this.props.postObj.type === 'help_request' ? 'helpReq' : 'helpSug'}`}>
+        <div className="post-icon"></div>
+        <div className="post-title">
+          <h3>{this.props.postObj.title}</h3>
+        </div>
+        <div className="post-content">
+          <p>{this.props.postObj.user.name}</p>
+          <p>{this.props.postObj.type}</p>
+          <p><b>Category:</b> {this.props.postObj.category.name}</p>
+          <p><b>Posted at:</b> {this.props.postObj.open_date}</p>
+          <p><b>Description:</b> {this.props.postObj.description}</p>
+          <p><b>Difficulty level:</b> {this.props.postObj.difficulty}</p>
+          <p><b>It will take</b> {this.props.postObj.duration} hours</p>
+          <p><b>Location:</b> {this.props.postObj.location}</p>
+          <p><b>{this.getTypeOfRequest()}</b> {this.props.postObj.number_of_helpers_needed}</p>
+          <p>It will give {this.props.postObj.score} point!</p>
+          <p>Status: {this.props.postObj.status}</p>
+        </div>
+
         {
-          this.props.postObj.comments.map((comment) =>
+          this.props.postObj.comments.map((comment, index) =>
             (
-              <Comment key={comment.toString()} commentObj={comment} />
+              <Comment key={index.toString()} commentObj={comment} />
             )
           )
         }
       </div>
     );
   }
+
 }
 
 Post.propTypes = {
