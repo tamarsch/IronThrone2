f072298168e1c88db9bf53c7ab0a909aa15735d3
tamar schachter <tam19sh991@gmail.com>
2017-11-30 09:01:28 +0200

    fix errors

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -5,33 +5,52 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 
-export class LoginPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export default class LoginPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
   render() {
     return (
       <div>
+        <MuiThemeProvider>
+          <div>
+            <AppBar
+              title="Login"
+            />
+            <TextField
+              hintText="Enter your Username"
+              floatingLabelText="Username"
+              onChange={(event, newValue) => this.setState({ username: newValue })}
+            />
+            <br />
+            <TextField
+              type="password"
+              hintText="Enter your Password"
+              floatingLabelText="Password"
+              onChange={(event, newValue) => this.setState({ password: newValue })}
+            />
+            <br />
+            <RaisedButton label="Submit" primary style={style} onClick={(event) => this.handleClick(event)} />
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
 }
-
-LoginPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+const style = {
+  margin: 15,
 };
 
+LoginPage.propTypes = {
+};
 
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
-
-const withConnect = connect(null, mapDispatchToProps);
-
-export default compose(
-  withConnect,
-)(LoginPage);
